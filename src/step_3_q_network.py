510559f2235e7b772bc28440d5fd64b05aed7335
Alvin Wan <hi@alvinwan.com>
2018-01-31 03:43:23 -0800

    finished step 3 code

--- src/step_3_q_network.py
+++ src/step_3_q_network.py
@@ -25,22 +25,22 @@ def main():
     global exploration_probability
     env = gym.make('FrozenLake-v0')  # create the game
     env.seed(0)  # make results reproducible
-    n_obs, n_actions = env.observation_space.n, env.action_space.n
     rewards = []
 
     # 1. Setup placeholders
-    obs_t_ph = tf.placeholder(shape=[1, 16], dtype=tf.float32)
-    obs_tp1_ph = tf.placeholder(shape=[1,16], dtype=tf.float32)
+    n_obs, n_actions = env.observation_space.n, env.action_space.n
+    obs_t_ph = tf.placeholder(shape=[1, n_obs], dtype=tf.float32)
+    obs_tp1_ph = tf.placeholder(shape=[1, n_obs], dtype=tf.float32)
     act_ph = tf.placeholder(tf.int32, shape=())
     rew_ph = tf.placeholder(shape=(), dtype=tf.float32)
-    Q2_tp1_ph = tf.placeholder(shape=[1, 4], dtype=tf.float32)
+    q_target_ph = tf.placeholder(shape=[1, n_actions], dtype=tf.float32)
 
     # 2. Setup computation graph
-    W = tf.Variable(tf.random_uniform([16, 4], 0, 0.01))
+    W = tf.Variable(tf.random_uniform([n_obs, n_actions], 0, 0.01))
     q_current = tf.matmul(obs_t_ph, W)
     q_target = tf.matmul(obs_tp1_ph, W)
 
-    q_target_max = tf.reduce_max(Q2_tp1_ph, axis=1)
+    q_target_max = tf.reduce_max(q_target_ph, axis=1)
     q_target_sa = rew_ph + discount_factor * q_target_max
     q_current_sa = q_current[0, act_ph]
     error = tf.reduce_sum(tf.square(q_target_sa - q_current_sa))
@@ -51,30 +51,32 @@ def main():
     update_model = trainer.minimize(error)
 
     with tf.Session() as session:
-        session.run(tf.initialize_all_variables())
+        session.run(tf.global_variables_initializer())
 
         for episode in range(1, num_episodes + 1):
             obs_t = env.reset()
             episode_reward = 0
             while True:
-                obs_t_oh = one_hot(obs_t, n_obs)
 
                 # 4. Take step using best action or random action
+                obs_t_oh = one_hot(obs_t, n_obs)
                 action = session.run(pred_act_ph, feed_dict={obs_t_ph: obs_t_oh})[0]
                 if np.random.rand(1) < exploration_probability(episode):
                     action = env.action_space.sample()
                 obs_tp1, reward, done, _ = env.step(action)
 
                 # 5. Train model
-                obs_tp1_oh, obs_t = one_hot(obs_tp1, n_obs), obs_tp1
-                Q_tp1 = session.run(q_target, feed_dict={obs_tp1_ph: obs_tp1_oh})
+                obs_tp1_oh = one_hot(obs_tp1, n_obs)
+                q_target_val = session.run(q_target, feed_dict={obs_tp1_ph: obs_tp1_oh})
                 session.run(update_model, feed_dict={
                     obs_t_ph: obs_t_oh,
                     rew_ph: reward,
-                    Q2_tp1_ph: Q_tp1,
+                    q_target_ph: q_target_val,
                     act_ph: action
                 })
                 episode_reward += reward
+                obs_t = obs_tp1
+
                 if done:
                     rewards.append(episode_reward)
                     if episode % report_interval == 0:
@@ -84,6 +86,5 @@ def main():
         print('Average reward: %.2f' % np.mean(rewards))
 
 if __name__ == '__main__':
-    # pass
     main()
 
